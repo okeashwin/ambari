4722a0f266a67b5c4cc23c0ed9008a8e63f40f0b
Ashwin Oke <okeashwin@gmail.com>
2015-11-13 23:39:52 -0500

    Code Commenting

--- ambari-server/src/main/java/org/apache/ambari/server/agent/WebSocketClient.java
+++ ambari-server/src/main/java/org/apache/ambari/server/agent/WebSocketClient.java
@@ -22,33 +22,21 @@ import javax.websocket.*;
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
-/**
- * Created by nisarg on 10/25/15.
- */
 @ClientEndpoint
 public class WebSocketClient {
-    CountDownLatch latch = new CountDownLatch(1);
     private Session session;
 
     @OnOpen
     public void onOpen(Session session) {
-        System.out.println("Connected to server");
         this.session = session;
-        latch.countDown();
     }
 
     @OnMessage
     public void onText(String message, Session session) {
-        System.out.println("Message received from server:" + message);
     }
 
     @OnClose
     public void onClose(CloseReason reason, Session session) {
-        System.out.println("Closing a WebSocket due to " + reason.getReasonPhrase());
-    }
-
-    public CountDownLatch getLatch() {
-        return latch;
     }
 
     public void sendMessage(String str) {
